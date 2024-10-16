22ce2dfbd6245b4f4b48a6f19801b1e508bf018b
Josue Castro <josuecaztro@gmail.com>
2024-10-15 20:27:32 -0400

    backend configured

---
+++ wedding-server/src/main/java/wedding_planner_app/wedding_planner/WeddingPlannerApplication.java
@@ -0,0 +1,13 @@
+package wedding_planner_app.wedding_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeddingPlannerApplication.class, args);
+	}
+
+}
---
+++ wedding-server/src/test/java/wedding_planner_app/wedding_planner/WeddingPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package wedding_planner_app.wedding_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
